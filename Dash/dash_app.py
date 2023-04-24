ecc988cafc3f4f18204564dfd42131b17bc02f11
Madster973 <50129506+Madster973@users.noreply.github.com>
2023-04-24 01:05:07 -0400

    Update dash_app.py

--- Dash/dash_app.py
+++ Dash/dash_app.py
@@ -31,11 +31,12 @@ FRED_INDICATORS = ['GDP', 'GDPC1', 'GDPPOT', 'NYGDPMKTPCDWLD',         # 1. Grow
 ETF_INDICATORS = ['VDE.US','VHT.US']
 
 app = dash.Dash(__name__)
+server = app.server
 auth = dash_auth.BasicAuth(
     app,
     VALID_USERNAME_PASSWORD_PAIRS
 )
-server = app.server
+
 
 # Define app layout with a navigation bar and empty page content
 app.layout = html.Div([
